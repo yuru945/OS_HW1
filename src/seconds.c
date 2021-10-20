b0e57236e44d7ee3fc26ce14fd776630a860ca5c
yuru945 <105306042@g.nccu.edu.tw>
2021-10-20 06:51:59 +0000

    first commit

--- src/seconds.c
+++ src/seconds.c
@@ -6,7 +6,7 @@
 #include <linux/uaccess.h>
 #include <asm/uaccess.h>
 #include <linux/jiffies.h> // 裡裡面宣告了jiffies變數
-
+#include <asm/param.h> //定義HZ
 
 #define BUFFER_SIZE 128
 
@@ -41,7 +41,7 @@ static int proc_init(void)
 {
     
     //////////////////////
-    ////<在此加入程式碼>////
+    unsigned long init_jiffies = jiffies;
     /////////////////////
     //用變數記住載入模組時的jiffies
     
@@ -81,10 +81,12 @@ static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, 
 
         // 計算目前jiffies和模組初始jiffies的差，並除以HZ值
         // 將結果透過sprintf放到buffer中，使用%ld印出
+        unsigned long total_elapsed_time = jiffies;
+        int hz = HZ;
         rv = sprintf(buffer,
                      "Module has been running for %ld seconds\n",
                      //////////////////////
-                     ////<在此加入程式碼>////
+                     ((total_elapsed_time - init_jiffies)/hz)
                      /////////////////////
                      );
 
